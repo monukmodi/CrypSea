f7dd3b5b71d710d526a7645f6e9a6e329cf8126d
monukmodi <monukr2012@gmail.com>
2022-01-31 13:34:59 +0530

    Crypto Price

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ExchangeRates from './pages/ExchangeRates/ExchangeRates'
 import Header from './components/Header/Header'
 
